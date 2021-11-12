54b75ae328960283653c6b983de90cfaf1324f30
Yago Carro <ycarro@c2r3s1.42madrid.com>
2021-11-12 16:51:57 +0100

    1211_1

--- src/args.c
+++ src/args.c
@@ -1,19 +1,67 @@
-void	checkargs(int argc, char **argv)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycarro <ycarro@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 11:07:44 by ycarro            #+#    #+#             */
+/*   Updated: 2021/11/12 12:14:22 by ycarro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+int	checkargs(int argc, char **argv);
+int	divide(char *line);
+int main(int argc, char **argv);
+
+
+int	checkargs(int argc, char **argv)
 {
+	int	sec;
 	int	tmp;
 	int	i;
 
+	sec = argc - 1;
 	tmp = 0;
-	while(++tmp < argc)
+	while (++tmp < argc)
 	{
-
-		while(argv[tmp][i])
+		i = -1;
+		while (argv[tmp][++i])
 		{
 			if (argv[tmp][i] == ' ')
-				divide()
-			i++;
+			{
+				sec += (divide(argv[tmp])) - 1;
+				break;
+			}
 		}
-		argc++;
-
 	}
+	return (sec);
+}
+
+int	divide(char *line)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (*(line) != ' ')
+		cnt++;
+	while (*line)
+	{
+		if (*line == ' ' && *(line + 1) != ' ')
+			if (*(line + 1))
+				cnt++;
+		line++;
+	}
+	return (cnt);
+}
+
+int main(int argc, char **argv)
+{
+	int words;
+
+	words = checkargs(argc, argv);
+	printf("%d\n", words);
+	return 0;
 }
